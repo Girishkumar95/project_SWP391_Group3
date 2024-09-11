d8d8001b5daecdb8ea037c77299894911b0b5e69
Daitqne <daitqhe176017@fpt.edu.vn>
2024-09-11 17:53:51 +0700

    DaiTQ
    Signed-off-by: Daitqne <daitqhe176017@fpt.edu.vn>

---
+++ CarShopSales/src/test/java/com/poly/carshopsales/CarShopSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.carshopsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
